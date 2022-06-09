c57989bfe070217752687e00f143d83744abbd02
John Borges <105023503+JohnBorges52@users.noreply.github.com>
2022-06-08 21:20:40 -0700

    Update wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  let result;
+  for (l of horizontalJoin) {
+    if (l === word) {
+      return true
+     
     }
-}
+  
+  
+  }
 
-module.exports = wordSearch
+
+  let array = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let col = [];
+    for (let j = 0; j < letters.length; j++) {
+      col.push(letters[j][i]);
+    }
+    array.push(col);
+  }
+  for (let index in array) {
+    const arrayJoin = array[index].join("");
+    if (arrayJoin.includes(word)) {
+      return true;
+    }
+  }
+  return false
+};
+
+
+module.exports = wordSearch;
